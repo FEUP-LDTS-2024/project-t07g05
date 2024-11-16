18bb1ea0e9464074abbfa9c0691b36414a99ffcd
Margarida Ribeiro <up202305264@g.uporto.pt>
2024-11-16 23:13:38 +0000

    Add more tests for Position

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -25,7 +25,38 @@ public class PositionTest {
     }
 
 
+    @Test
+    public void testEqualsDifferentObjectSameValues() {
+
+        Position position1 = new Position(5, 10);
+        Position position2 = new Position(5, 10);
+        Assertions.assertEquals(position1, position2, "Positions with the same coordinates should be equal.");
+    }
 
 
+    @Test
+    public void testEqualsDifferentCoordinates() {
+
+        Position position1 = new Position(5, 10);
+        Position position2 = new Position(7, 10);
+        Assertions.assertNotEquals(position1, position2, "Positions with different coordinates should not be equal.");
+    }
+
+
+    @Test
+    public void testEqualsNull() {
+
+        Position position = new Position(5, 10);
+        Assertions.assertNotEquals(position, null, "Position should not be equal to null.");
+    }
+
+
+    @Test
+    public void testEqualsDifferentClass() {
+
+        Position position = new Position(5, 10);
+        String otherObject = "Not a Position";
+        Assertions.assertNotEquals(position, otherObject, "Position should not be equal to an object of a different class.");
+    }
 
 }
