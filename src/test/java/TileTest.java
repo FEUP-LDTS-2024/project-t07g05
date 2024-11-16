9db0a076795374a7dd35755855a86588a4f39668
Margarida Ribeiro <up202305264@g.uporto.pt>
2024-11-16 20:45:20 +0000

    Add test for Symbol determination

--- src/test/java/TileTest.java
+++ src/test/java/TileTest.java
@@ -9,14 +9,17 @@ public class TileTest {
 
     @BeforeEach
     public void setUp(){
+
         Position screenPosition = new Position(0,0);
         Position gridCoordinates = new Position(0,0);
+
         tile = new Tile("jewel", "ruby", screenPosition, gridCoordinates);
     }
 
 
     @Test
     public void testCursorOn(){
+
         tile.setCursorOn(true);
         Assertions.assertTrue(tile.isCursorOn(), "The cursor should be on");
 
@@ -27,17 +30,31 @@ public class TileTest {
 
     @Test
     public void testColorChange(){
-        tile.setColor("emerald");
-        Assertions.assertEquals("#11a62f", tile.getColor(), "The tile color should be emerald.");
+
+        tile.setColor("#11a62f");
+
+        Assertions.assertEquals("#11a62f", tile.getColor(), "The tile color should be updated to the specific color.");
     }
 
 
     @Test
     public void testTileInitialization(){
+
         Assertions.assertNotNull(tile, "Tile should be initialized.");
+
         Assertions.assertEquals("ruby", tile.getColor(), "The tile color should be ruby.");
+
         Assertions.assertEquals("◼", tile.getSymbol(), "The tile should be ◼.");
     }
 
 
+    @Test
+    public void testSymbolDetermination(){
+
+        Tile bombTile = new Tile("bomb", "emerald", new Position(1,1), new Position(1,1));
+        Assertions.assertEquals("⊖", bombTile.getSymbol(), "Tile symbol should be ⊖ for bombs");
+
+        Tile defaultTile = new Tile("default", "ruby", new Position(0, 0), new Position(0, 0));
+        Assertions.assertEquals("◼", defaultTile.getSymbol(), "The tile symbol should be ◼ for default tiles.");
+    }
 }
