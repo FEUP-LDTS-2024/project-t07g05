76e0dd4d37375c830c01bd570f3f765f756ae2b2
Margarida Ribeiro <up202305264@g.uporto.pt>
2024-11-16 12:04:47 +0000

    Added test for Tile initialization in Board

--- src/main/java/BoardTest.java
+++ src/main/java/BoardTest.java
@@ -16,9 +16,28 @@ public class BoardTest {
                 Tile tile = board.getTile(row, col);
 
                 Assertions.assertNotNull(tile, "The tile at position (" + row + ", " + col + ") should not be null.");
+            }
+        }
+    }
 
+
+    @Test
+    public void testTileInitialization(){
+
+        Board board = new Board(8,10,100, 40,4,4);
+
+        String[] validColors = {"diamond", "ruby", "emerald", "sapphire", "amethyst"};
+
+        for (int row = 0; row < board.getRows(); row++){
+            for (int col = 0; col < board.getColumns(); col ++){
+                Tile tile = board.getTile(row, col);
+
+                Assertions.assertEquals("jewel", tile.getType(), "Tile type should be jewel.");
+
+                Assertions.assertTrue(java.util.Arrays.asList(validColors).contains(tile.getColor()), "Tile color at position (" + row + ", " + col + ") should be valid.");
             }
         }
     }
 }
+
 
