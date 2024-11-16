e6df7226770140e70fe3552c81446be6b5cee7ca
Margarida Ribeiro <up202305264@g.uporto.pt>
2024-11-16 18:52:06 +0000

    Create TileTest class with cursor and color change tests

---
+++ src/main/java/TileTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class TileTest {
+
+    private Tile tile;
+
+    @BeforeEach
+    public void setUp(){
+        Position screenPosition = new Position(0,0);
+        Position gridCoordinates = new Position(0,0);
+        tile = new Tile("jewel", "ruby", screenPosition, gridCoordinates);
+    }
+
+
+    @Test
+    public void testCursorOn(){
+        tile.setCursorOn(true);
+        Assertions.assertTrue(tile.isCursorOn(), "The cursor should be on");
+
+        tile.setCursorOn(false);
+        Assertions.assertFalse(tile.isCursorOn(), "The cursor should be off");
+    }
+
+
+    @Test
+    public void testColorChange(){
+        tile.setColor("emerald");
+        Assertions.assertEquals("#11a62f", tile.getColor(), "The tile color should be emerald.");
+    }
+
+
+}
