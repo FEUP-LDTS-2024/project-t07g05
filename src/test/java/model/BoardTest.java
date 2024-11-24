ad9a4e4f2d5796ef2033ae61f678c816c7abd840
Margarida Ribeiro <up202305264@g.uporto.pt>
2024-11-24 18:03:50 +0000

    Added more tests

--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -1,4 +1,45 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BoardTest {
+
+    @Test
+    void testBoardInitialization(){
+        Board board = new Board(5,5,800,600,10,10);
+        assertEquals(5, board.getRows());
+        assertEquals(5, board.getColumns());
+        assertEquals(800, board.getWidth());
+        assertEquals(600, board.getHeight());
+        assertNotNull(board.getGrid());
+        assertNotNull(board.getCurrentTile());
+    }
+
+    @Test
+    void testTileAccess(){
+        Board board = new Board(3,3,800,600,10,10);
+        Tile tile = board.getTile(1,1);
+        assertNotNull(tile);
+        assertEquals(1, tile.getGridCoordinates().getX());
+        assertEquals(1, tile.getGridCoordinates().getY());
+    }
+
+    @Test
+    void testIsValidPosition(){
+        Board board = new Board(4,4,800,600,10,10);
+        assertTrue(board.isValidPosition(2,2));
+        assertFalse(board.isValidPosition(5,5));
+    }
+
+    @Test
+    void testCalculateScreenPosition(){
+        Board board = new Board(5,5,800,600,10,10);
+        Position gridPosition = new Position(2,3);
+        Position screenPosition = board.calculateScreenPosition(gridPosition);
+
+        assertEquals(3 * 10 + board.getStartX(), screenPosition.getX());
+        assertEquals(2 * 10 + board.getStartY(), screenPosition.getY());
+    }
 }
--- src/test/java/model/MatchHandlerTest.java
+++ src/test/java/model/MatchHandlerTest.java
@@ -1,4 +1,43 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MatchHandlerTest {
+    @Test
+    void testFindMatches(){
+        Board board = new Board(5,5,800, 600,10,10);
+        MatchHandler handler = new MatchHandler(board);
+
+        Tile matchTile = new Tile("jewel", "ruby", new Position(0,0), new Position(0,0));
+        board.setTile(0,0, matchTile);
+        board.setTile(0,1, matchTile);
+        board.setTile(0,2, matchTile);
+
+        handler.findMatches();
+        List<Tile> matches = handler.matches;
+
+        assertEquals(3, matches.size());
+    }
+
+    @Test
+    void testPopMatches(){
+        Board board = new Board(5,5,800, 600,10,10);
+        MatchHandler handler = new MatchHandler(board);
+
+        Tile matchTile = new Tile("jewel", "ruby", new Position(0,0), new Position(0,0));
+        board.setTile(0,0, matchTile);
+        board.setTile(1,0, matchTile);
+        board.setTile(2,0, matchTile);
+
+        handler.findMatches();
+        handler.popMatches();
+
+        assertInstanceOf(EmptyTile.class, board.getTile(0, 0));
+        assertInstanceOf(EmptyTile.class, board.getTile(1, 0));
+        assertInstanceOf(EmptyTile.class, board.getTile(2, 0));
+
+    }
 }
