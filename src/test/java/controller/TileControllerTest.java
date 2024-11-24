c2ff449fe278e033b77c357411b0c54fbe12ac4e
Margarida Ribeiro <up202305264@g.uporto.pt>
2024-11-24 16:27:34 +0000

    Updated TileControllerTest

--- src/test/java/controller/TileControllerTest.java
+++ src/test/java/controller/TileControllerTest.java
@@ -1,4 +1,28 @@
 package controller;
 
+import model.Board;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.Mockito;
+
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class TileControllerTest {
+    private TileController tileController;
+
+    @BeforeEach
+    void setUp() {
+        Board mockBoard = Mockito.mock(Board.class);
+        tileController = new TileController(mockBoard);
+    }
+
+    @Test
+    void testStepDoesNotThrowException() {
+        assertDoesNotThrow(() -> tileController.step());
+
+    }
 }
