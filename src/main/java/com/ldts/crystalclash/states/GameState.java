d5ca3f1d1569121558814e3eb581b470d2acff2e
Henrique <up202306582@up.pt>
2024-12-12 18:14:25 +0000

    creating states

---
+++ src/main/java/com/ldts/crystalclash/states/GameState.java
@@ -0,0 +1,27 @@
+package com.ldts.crystalclash.states;
+
+
+import com.ldts.crystalclash.controller.BoardController;
+import com.ldts.crystalclash.controller.Controller;
+
+import com.ldts.crystalclash.model.Board;
+import com.ldts.crystalclash.viewer.GameViewer;
+import com.ldts.crystalclash.viewer.Viewer;
+
+
+
+public class GameState extends State<Board>{
+    public GameState(Board board) {
+        super(board);
+    }
+
+    @Override
+    protected Viewer<Board> getViewer() {
+        return new GameViewer(getModel());
+    }
+
+    @Override
+    protected Controller<Board> getController() {
+        return new BoardController(getModel());
+    }
+}
