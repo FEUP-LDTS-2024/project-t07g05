ed681bbba00af9b1cec8d7d07b53e6a2933a9033
Margarida Ribeiro <up202305264@g.uporto.pt>
2024-12-17 17:06:57 +0000

    Changed GameControllerTest

--- src/main/java/com/ldts/crystalclash/model/EmptyTile.java
+++ src/main/java/com/ldts/crystalclash/model/EmptyTile.java
@@ -2,11 +2,9 @@ package com.ldts.crystalclash.model;
 
 public class EmptyTile extends Tile {
     public EmptyTile(Position screenPosition, Position gridCoordinates, Color color) {
+
         super(screenPosition, gridCoordinates, color);
         this.symbol = " ";
     }
-
-    public EmptyTile(Position position, Position position1) {
-        super();
-    }
 }
+
--- src/test/java/controller/GameControllerTest.java
+++ src/test/java/controller/GameControllerTest.java
@@ -1,36 +1,33 @@
 package controller;
-
-import com.ldts.crystalclash.Game;
 import com.ldts.crystalclash.controller.BoardController;
 import com.ldts.crystalclash.controller.GameController;
 import com.ldts.crystalclash.gui.GUI;
 import com.ldts.crystalclash.model.Board;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import com.ldts.crystalclash.Game;
 import static org.mockito.Mockito.*;
+
 
 public class GameControllerTest {
     private GameController controller;
-    private BoardController boardController;
-    private Board board;
 
     @BeforeEach
     void setUp() {
-        board = mock(Board.class);
-        boardController = mock(BoardController.class);
+
+        Board board = mock(Board.class);
         controller = new GameController(board);
     }
 
     @Test
     void testStep() throws Exception {
+
         BoardController boardController = mock(BoardController.class);
+
         GameController spyController = spy(controller);
 
-        doReturn(boardController).when(spyController).getModel();
+        spyController.step(mock(Game.class), GUI.ACTION.UP, 0);
 
-        spyController.step(mock(Game.class), GUI.ACTION.UP);
-
-        verify(boardController).step(any(Game.class), eq(GUI.ACTION.UP));
+        verify(boardController).step(any(Game.class), eq(GUI.ACTION.UP), anyLong());
     }
 }
