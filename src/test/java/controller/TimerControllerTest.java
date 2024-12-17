7865fff2f8224f1022cbcd554136a786b13b1432
Margarida Ribeiro <up202305264@g.uporto.pt>
2024-12-17 17:19:39 +0000

    Changed TileControllerTest

--- src/test/java/controller/TimerControllerTest.java
+++ src/test/java/controller/TimerControllerTest.java
@@ -1,4 +1,44 @@
 package controller;
 
-public class TimerControllerTest {
+import com.ldts.crystalclash.controller.TimerController;
+import com.ldts.crystalclash.model.Timer;
+import com.ldts.crystalclash.Game;
+import com.ldts.crystalclash.gui.GUI;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class TimerControllerTest {
+
+    private TimerController controller;
+    private Timer timer;
+
+    @BeforeEach
+    void setUp() {
+        timer = mock(Timer.class);
+        controller = new TimerController(timer);
+    }
+
+    @Test
+    void testConstructor() {
+        assert controller != null;
+    }
+
+    @Test
+    void testStartTimer() {
+        controller.startTimer();
+
+        verify(timer).start();
+    }
+
+    @Test
+    void testStep() throws Exception {
+        Game game = mock(Game.class);
+        GUI.ACTION action = GUI.ACTION.NONE;
+
+        controller.step(game, action, 0);
+        
+        verifyNoInteractions(timer);
+    }
 }
