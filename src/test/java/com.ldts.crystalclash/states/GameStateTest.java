c16f0e4f3b52d3a54b41e1e0d4e55e7020c540f0
Margarida Ribeiro <up202305264@g.uporto.pt>
2024-12-24 03:24:39 +0000

    Added tests to GameStateTest

--- src/test/java/com.ldts.crystalclash/states/GameStateTest.java
+++ src/test/java/com.ldts.crystalclash/states/GameStateTest.java
@@ -1,15 +1,18 @@
 package com.ldts.crystalclash.states;
 
 import com.ldts.crystalclash.Game;
+import com.ldts.crystalclash.controller.Controller;
 import com.ldts.crystalclash.model.Board;
 import com.ldts.crystalclash.viewer.GameViewer;
 import com.ldts.crystalclash.controller.GameController;
 import com.ldts.crystalclash.gui.GUI;
+import com.ldts.crystalclash.viewer.Viewer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class GameStateTest {
@@ -54,6 +57,32 @@ class GameStateTest {
         verify(mockController, times(1)).step(mockGame, GUI.ACTION.UP, time);
         verify(mockViewer, times(1)).draw(mockGUI);
         verify(mockGUI, times(1)).getNextAction();
+    }
+    @Test
+    void testViewerInitialization() {
+        Viewer<Board> viewer = gameState.getViewer();
+        assertNotNull(viewer, "Viewer should not be null");
+        assertInstanceOf(GameViewer.class, viewer, "Viewer should be an instance of GameViewer");
+    }
+
+    @Test
+    void testControllerInitialization() {
+        Controller<Board> controller = gameState.getController();
+        assertNotNull(controller, "Controller should not be null");
+        assertInstanceOf(GameController.class, controller, "Controller should be an instance of GameController");
+    }
+
+    @Test
+    void testStepHandlesNullAction() throws IOException {
+        long time = 100L;
+        Game mockGame = mock(Game.class);
+
+        when(mockGUI.getNextAction()).thenReturn(null);
+
+        gameState.step(mockGame, mockGUI, time);
+
+        verify(mockController, times(1)).step(mockGame, null, time);
+        verify(mockViewer, times(1)).draw(mockGUI);
     }
 
 }
